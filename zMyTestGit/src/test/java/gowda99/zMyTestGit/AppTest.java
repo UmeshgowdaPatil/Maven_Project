7c75e09d1811c6e26f13b78f3a849f17768fd449
GOWDRU <GOWDRU@GOWDRU-PC>
2017-03-14 16:11:47 +0530

    Added ChromeDriver code in AppTest.Java

--- zMyTestGit/src/test/java/gowda99/zMyTestGit/AppTest.java
+++ zMyTestGit/src/test/java/gowda99/zMyTestGit/AppTest.java
@@ -2,8 +2,11 @@ package gowda99.zMyTestGit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
 
 /**
@@ -14,17 +17,17 @@ public class AppTest {
 	@Test
 	public void testG() throws InterruptedException{
 		
-		FirefoxProfile profile = new FirefoxProfile();
-		profile.setAcceptUntrustedCertificates(true);
-		profile.setAssumeUntrustedCertificateIssuer(false);
-		System.setProperty("webdriver.gecko.driver", ".//webdriver//geckodriver.exe");
-		WebDriver driver = new FirefoxDriver(profile);
-		System.out.println("Firefox Instantiated");
+//		FirefoxProfile profile = new FirefoxProfile();
+//		profile.setAcceptUntrustedCertificates(true);
+//		profile.setAssumeUntrustedCertificateIssuer(false);
+//		System.setProperty("webdriver.gecko.driver", ".//webdriver//geckodriver.exe");
+//		WebDriver driver = new FirefoxDriver(profile);
+//		System.out.println("Firefox Instantiated");
 		
-//		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-//		capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
-//		System.setProperty("webdriver.chrome.driver", ".//webdriver//chromedriver.exe");
-//		WebDriver driver = new ChromeDriver(capabilities);
+		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+		capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+		System.setProperty("webdriver.chrome.driver", ".//webdriver//chromedriver.exe");
+		WebDriver driver = new ChromeDriver(capabilities);
 		
 //		DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
 //		capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
