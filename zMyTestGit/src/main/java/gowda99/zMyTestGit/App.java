e8f368cf22e6212084307caaeb1de0d4c58239ee
UmeshgowdaPatil <umeshgowda027@gmail.com>
2017-03-14 15:02:07 +0530

    First time saving select all click commit only

---
+++ zMyTestGit/src/main/java/gowda99/zMyTestGit/App.java
@@ -0,0 +1,13 @@
+package gowda99.zMyTestGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ zMyTestGit/src/test/java/gowda99/zMyTestGit/AppTest.java
@@ -0,0 +1,43 @@
+package gowda99.zMyTestGit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+        
+	@Test
+	public void testG() throws InterruptedException{
+		
+		FirefoxProfile profile = new FirefoxProfile();
+		profile.setAcceptUntrustedCertificates(true);
+		profile.setAssumeUntrustedCertificateIssuer(false);
+		System.setProperty("webdriver.gecko.driver", ".//webdriver//geckodriver.exe");
+		WebDriver driver = new FirefoxDriver(profile);
+		
+//		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+//		capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+//		System.setProperty("webdriver.chrome.driver", ".//webdriver//chromedriver.exe");
+//		WebDriver driver = new ChromeDriver(capabilities);
+		
+//		DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
+//		capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+//		System.setProperty("webdriver.ie.driver", ".//webdriver//IEDriverServer.exe");
+//		WebDriver driver = new InternetExplorerDriver(capabilities);
+		
+		
+		driver.get("https://www.google.co.in/");
+		driver.findElement(By.id("lst-ib")).sendKeys("sachin");
+		driver.findElement(By.id("_fZl")).click();
+		System.out.println(driver.getTitle());
+		Thread.sleep(4000);
+		driver.close();
+		
+	}
+     
+}
